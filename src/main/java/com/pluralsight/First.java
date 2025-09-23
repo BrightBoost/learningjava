3f1a155fde296a707159db0c7891786d2b52fd39
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-09-23 20:45:49 +0200

    update

--- src/main/java/com/pluralsight/First.java
+++ src/main/java/com/pluralsight/First.java
@@ -1,8 +1,22 @@
 package com.pluralsight;
 
 public class First {
+    static int h = 9;
     public static void main(String[] args) {
         // outputting the word Hi!
         System.out.println("Hi!");
+        int x = 0;
+        byte b = 119;
+        System.out.println(x);
+        char c = 'a';
+        System.out.println((char)(c + 1));
+        String s = "text";
+
+        if(true) {
+            int y = 8;
+            System.out.println(y);
+            System.out.println(x);
+        }
+        System.out.println(h);
     }
 }
