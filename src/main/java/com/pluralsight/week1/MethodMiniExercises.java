bbf9ed90aabb528726fc6b5438106a7b5b54d2ef
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-09-26 23:14:50 +0200

    update

--- src/main/java/com/pluralsight/week1/MethodMiniExercises.java
+++ src/main/java/com/pluralsight/week1/MethodMiniExercises.java
@@ -4,6 +4,8 @@ public class MethodMiniExercises {
     public static void main(String[] args) {
         printWelcome();
         displayPlayerName("Mieraf");
+        String playerTitle = getPlayerTitle("Mieraf");
+        System.out.println(playerTitle);
     }
 
     /*
@@ -20,5 +22,19 @@ public class MethodMiniExercises {
 
     public static void displayPlayerName(String playerName) {
         System.out.println("Player joined: " + playerName);
+    }
+
+    /*
+    From second file
+    ### **1. public static String getPlayerTitle(String name)**
+> Return a string like:
+`"Lord " + name` or `"Princess " + name"`
+→ Call it in `main()` and print the result.
+     */
+
+    public static String getPlayerTitle(String name) {
+        // you could have more code here
+        // for example print something
+        return "Princess " + name;
     }
 }
