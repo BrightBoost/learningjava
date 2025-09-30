e6f92d7d669b8c7ecc454a197a971ff9e52ebada
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-09-30 22:12:56 +0200

    update

--- src/main/java/com/pluralsight/week2/FullNameParser.java
+++ src/main/java/com/pluralsight/week2/FullNameParser.java
@@ -2,11 +2,12 @@ package com.pluralsight.week2;
 
 public class FullNameParser {
     public static void main(String[] args) {
-        fullNameParser1("Maaike Putten");
+        fullNameParser2("Maaike Van Putten");
     }
 
-    //without split - Dana Wyatt
+    //without split
     public static void fullNameParser1(String name) {
+        name = name.trim();
         // variables for name parts
         String firstName = "";
         String middleName = "(none)";
@@ -28,13 +29,47 @@ public class FullNameParser {
             lastName = name.substring(space2Index + 1);
         }
 
-        System.out.println("First name: " + firstName);
-        System.out.println("Middle name: " + middleName);
-        System.out.println("Last name: " + lastName);
+        printName(firstName, middleName, lastName);
 
     }
     // with split
     public static void fullNameParser2(String name) {
+        name = name.trim();
+
+        // variables for name parts
+        String firstName = "";
+        String middleName = "(none)";
+        String lastName = "";
+
+        // splitting the name and store in an array
+        String[] arrFullName = name.split("\\s+");
+
+        // check the length of the array
+        // if length equal to 3, populate all the fields
+        // else if length equal to 2, populate first name and last name
+        // else invalid input
+
+        if(arrFullName.length  == 3) {
+            firstName = arrFullName[0];
+            middleName = arrFullName[1];
+            lastName = arrFullName[2];
+        } else if(arrFullName.length  == 2) {
+            firstName = arrFullName[0];
+            lastName = arrFullName[1];
+        } else {
+            System.out.println("Invalid name format!");
+            return;
+        }
+
+        // print the name
+        printName(firstName, middleName, lastName);
+
+    }
+
+    public static void printName(String firstName, String middleName, String lastName) {
+        System.out.println("First name: " + firstName);
+        System.out.println("Middle name: " + middleName);
+        System.out.println("Last name: " + lastName);
 
     }
 }
