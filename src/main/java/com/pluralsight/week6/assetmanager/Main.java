517e0671e7ae35d08a999af0fe96fceaff8304fe
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-10-29 00:00:09 +0100

    update

--- src/main/java/com/pluralsight/week6/assetmanager/Main.java
+++ src/main/java/com/pluralsight/week6/assetmanager/Main.java
@@ -1,10 +1,11 @@
 package com.pluralsight.week6.assetmanager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Asset> assets = new ArrayList<>();
+        List<Asset> assets = new ArrayList<>();
         House house1 = new House("main home", "2020-04-04", 200_000, "blabla street 123", 1, 100, 100);
         House house2 = new House("vacation home", "2021-04-04", 100_000, "lala street 123", 1, 50, 50);
         Vehicle vehicle1 = new Vehicle("main car", "yesterday", 10000, "toyota yaris", 2018, 10000);
