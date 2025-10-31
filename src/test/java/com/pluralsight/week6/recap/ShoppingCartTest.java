47db5878c243d192346617fcb1cc2fa802996789
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-10-31 22:19:56 +0100

    update

--- src/test/java/com/pluralsight/week6/recap/ShoppingCartTest.java
+++ src/test/java/com/pluralsight/week6/recap/ShoppingCartTest.java
@@ -37,5 +37,20 @@ class ShoppingCartTest {
 
     @Test
     void calcTotal() {
+        // arrange
+        ShoppingCart shoppingCart = new ShoppingCart("thomas");
+        Item item = new Item("keyboard", 50);
+        shoppingCart.addItem(item);
+        Item item2 = new Item("mouse", 80);
+        shoppingCart.addItem(item2);
+        Item item3 = new Item("cable", 20);
+        shoppingCart.addItem(item3);
+        double expectedTotal = 150;
+
+        // act
+        double resultTotal = shoppingCart.calcTotal();
+
+        // assert
+        assertEquals(expectedTotal, resultTotal);
     }
 }
